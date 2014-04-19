d50e8fe1a413a2c10707fbc5340b2c097d422fd5
git-us3r <mlink3.14@gmail.com>
2014-04-19 02:09:26 -0600

    Bomb is fully OO!

--- scripts/Bomb.js
+++ scripts/Bomb.js
@@ -2,6 +2,7 @@ var Bomb = (function(){
 
 	var uniqueId = 0;
 
+
 	// _glassNumbers contains an ordered array with the glass numbers to be used in countdount
 	// It is an array of Image objects.
 
@@ -9,80 +10,79 @@ var Bomb = (function(){
 	//////
 	function create(_notify, _bombImage, _glassNumbers, _checkMark, _expMark, _width, _height, _center, _rotation, _duration, _visible){
 
-		var bomb = Spec.create(_bombImage, _width, _height, _center, _rotation);
+		var bomb = {}
+			, bombImage = _bombImage
+			, checkMark = _checkMark
+			, expMark = _expMark
+			, duration = _duration
+			, currentTime = _duration
+			, visible = _visible
+			, glassNumbers = _glassNumbers
+			, currentGlassNumber = 0
+			, currentCover = glassNumbers[currentGlassNumber]
+			, maxGlassNumber = glassNumbers.length
+			, on = true
+			, safe = false
+			, safeRequest = false
+			, exp = false
+			, secondsCtr = 0;
 
 
+		// Set bomb with spec interface, mark it and increment uniqueId ctr.	
+		bomb = Spec.create(_bombImage, _width, _height, _center, _rotation);
 		bomb.id = uniqueId;
 		uniqueId++;
-
-		bomb.bombImage = _bombImage;
-		bomb.checkMark = _checkMark;
-		bomb.expMark = _expMark;
-		bomb.duration = _duration;
-		bomb.currentTime = _duration;
-		bomb.visible = _visible;
-
-		bomb.glassNumbers = _glassNumbers;
-		bomb.currentGlassNumber = 0;
-		bomb.currentCover = bomb.glassNumbers[bomb.currentGlassNumber];
-		bomb.maxGlassNumber = _glassNumbers.length;
-
-		bomb.on = true;
-		bomb.safe = false;
-		bomb.safeRequest = false;
-		bomb.exp = false;
-		bomb.secondsCtr = 0;
 		
 
 
-		bomb.update = function(_elapsedTime){
+		function update(_elapsedTime){
 
-			bomb.secondsCtr += _elapsedTime;
+			secondsCtr += _elapsedTime;
 
-			if(bomb.on && bomb.secondsCtr > 1){
+			if(on && secondsCtr > 1){
 
-				bomb.secondsCtr = 0;
+				secondsCtr = 0;
 
 				// Check if it has been clicked.
-				if(bomb.safeRequest){
+				if(safeRequest){
 
-					bomb.safe = true;
+					safe = true;
 				}
 
 
-				if(bomb.safe){
+				if(safe){
 
-				 	bomb.on = false;
-				 	bomb.currentCover = bomb.checkMark;
+				 	on = false;
+				 	currentCover = checkMark;
 				}
-				else if (bomb.currentGlassNumber < bomb.maxGlassNumber){					
+				else if (currentGlassNumber < maxGlassNumber){					
 						
-					bomb.currentGlassNumber++;
+					currentGlassNumber++;
 
 					// Check bounds
-					if(bomb.currentGlassNumber < bomb.maxGlassNumber){
+					if(currentGlassNumber < maxGlassNumber){
 						
-						bomb.currentCover = bomb.glassNumbers[bomb.currentGlassNumber];
+						currentCover = glassNumbers[currentGlassNumber];
 					}
 					else{
 						// time has expired and the bomb is not safe: explode
-						bomb.on = false;
-						bomb.exp = true;
-						bomb.currentCover = bomb.expMark;
+						on = false;
+						exp = true;
+						currentCover = expMark;
 						_notify();
 					}
 				}
 				
 			}
-		};
+		}
 
 
 		// Rendering must happen unconditionally, because the canvas is cleared every frame.
-		bomb.render = function(_graphics){
+		function render(_graphics){
 
 			_graphics.drawImage(bomb);
 
-			bomb.image = bomb.currentCover;
+			bomb.image = currentCover;
 			bomb.width -= 40;
 			bomb.height -= 40;
 			bomb.center.x += 16;
@@ -90,15 +90,15 @@ var Bomb = (function(){
 
 			_graphics.drawImage(bomb);
 
-			bomb.image = bomb.bombImage;
+			bomb.image = bombImage;
 			bomb.width += 40;
 			bomb.height += 40;
 			bomb.center.x -= 16;
 			bomb.center.y -= 14;
-		};
+		}
 
 
-		bomb.hit = function(_clickLocation){
+		function hit(_clickLocation){
 
 			var differenceVector = Vector2d.subtract(_clickLocation, bomb.center)
 				, differenceVectorMagnitude = Vector2d.magnitude(differenceVector);
@@ -114,18 +114,20 @@ var Bomb = (function(){
 			// It will become safe during update.
 			if((differenceVectorMagnitude <= .35 * bomb.width)){
 
-				bomb.safeRequest = true;
+				safeRequest = true;
 			}
 
-		};
+		}
 
 
-
-
+		bomb.update = update;
+		bomb.render = render;
+		bomb.hit = hit;
+		bomb.on = function(){return on};
 		return bomb;
 	}
 
 	return {create : create};
 
 }());
-exports.Bomb = Bomb;
+//exports.Bomb = Bomb;
